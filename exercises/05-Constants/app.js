14974a8892ab479ce92e2821fc98dc2cbc71c688
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-04-05 17:34:32 +0000

    First Commit

--- exercises/05-Constants/app.js
+++ exercises/05-Constants/app.js
@@ -1,5 +1,6 @@
-const VERSION = '0.1';
+// const VERSION = '0.1';
 
-VERSION = '0.9';
+const VERSION = '0.9';
+
 
 console.log(VERSION);
--- exercises/12-How_much_the_wedding_costs/app.js
+++ exercises/12-How_much_the_wedding_costs/app.js
@@ -1,11 +1,20 @@
 let guests = prompt('How many people are coming to your wedding?');
+guests = parseInt(guests);
 
 function getPrice(guests){
     let cost = 0;
     // Your code here
- 
+if (guests <= 50) {
+    cost = 4000
+}else if (guests <= 100) {
+    cost = 10000
+}else if (guests <= 200) {
+    cost = 15000
+}else {
+    cost = 20000
+}
 
-    return cost;
+return cost;
 }
 
 let price = getPrice(guests);
